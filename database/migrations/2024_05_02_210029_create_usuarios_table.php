3674e0c2d56855d5f01ae71abb308d4af2f92995
Miguesg95 <masglyl4@gmail.com>
2024-05-02 23:45:19 +0200

    Update 2024_05_02_210029_create_usuarios_table.php

--- database/migrations/2024_05_02_210029_create_usuarios_table.php
+++ database/migrations/2024_05_02_210029_create_usuarios_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string("password");
             $table->string("email");
             $table->boolean("admin")->default(false);
-            $table->foreignId('carrito_id')->nullable()->constrained();
         });
     }
 
