b5abc891fe12e5704f60a6385806673781015626
Miguesg95 <masglyl4@gmail.com>
2024-05-03 21:48:07 +0200

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreignId('rol_id')->nullable()->constrained();
+            $table->foreignId('rol_id')->nullable()->index();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
