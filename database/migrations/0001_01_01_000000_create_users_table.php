9e9d55c354bc42f07d73b4b0548a4bcd997f1f2c
Miguesg95 <masglyl4@gmail.com>
2024-05-03 19:37:38 +0200

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->boolean("admin")->default(false);
             $table->foreignId('rol_id')->nullable()->constrained();
         });
 
