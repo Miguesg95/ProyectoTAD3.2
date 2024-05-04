0a7a8d4f07d339688c0c638ea336d5bbeff360c8
Miguesg95 <masglyl4@gmail.com>
2024-05-04 14:32:47 +0200

    added stock to Calzado

--- database/migrations/2024_05_02_210018_create_calzados_table.php
+++ database/migrations/2024_05_02_210018_create_calzados_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->timestamps();
             $table->string("nombre");
             $table->string("marca");
+            $table->integer("stock");
             $table->string("descripcion");
             $table->double("precio");
         });
