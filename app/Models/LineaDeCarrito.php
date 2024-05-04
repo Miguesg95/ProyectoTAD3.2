4aaff1030bcf6955924d9dd729e36ddeee93653d
Miguesg95 <masglyl4@gmail.com>
2024-05-04 02:45:43 +0200

    updates Lineas

--- app/Models/LineaDeCarrito.php
+++ app/Models/LineaDeCarrito.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class LineaDeCarrito extends Model
 {
     use HasFactory;
+
+    public function carrito (){
+        return $this->belongsTo(Carrito::class);
+    }
+
+    public function calzado (){
+        return $this->hasOne(Calzado::class);
+    }
 }
--- app/Models/LineaDeVenta.php
+++ app/Models/LineaDeVenta.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class LineaDeVenta extends Model
 {
     use HasFactory;
+
+    public function venta (){
+        return $this->belongsTo(Venta::class);
+    }
+
+    public function calzado (){
+        return $this->hasOne(Calzado::class);
+    }
 }
