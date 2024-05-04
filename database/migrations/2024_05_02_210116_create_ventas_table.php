44079c8f5f069cbcf977799e038b9304359c2e8e
Miguesg95 <masglyl4@gmail.com>
2024-05-04 14:05:23 +0200

    added status to Venta

--- database/migrations/2024_05_02_210116_create_ventas_table.php
+++ database/migrations/2024_05_02_210116_create_ventas_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->double("importeTotal");
+            $table->string("estado");
             $table->foreignId('user_id')->nullable()->constrained();
         });
     }
